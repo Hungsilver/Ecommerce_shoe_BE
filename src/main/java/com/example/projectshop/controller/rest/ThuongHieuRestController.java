58a01a460ffdada95d869c0879f9e0272cde7baf
chien210030 <chien210030@gmail.com>
2023-10-18 12:58:23 +0700

    thuong hieu

--- src/main/java/com/example/projectshop/controller/rest/ThuongHieuRestController.java
+++ src/main/java/com/example/projectshop/controller/rest/ThuongHieuRestController.java
@@ -1,17 +1,16 @@
 package com.example.projectshop.controller.rest;
 
+import com.example.projectshop.dto.thuonghieu.ThuongHieuRequest;
+import com.example.projectshop.dto.thuonghieu.ThuongHieuResponse;
 import com.example.projectshop.service.IThuongHieuService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/thuonghieu")
+@RequestMapping("/api/thuong-hieu")
 public class ThuongHieuRestController {
 
     @Autowired
@@ -24,7 +23,24 @@ public class ThuongHieuRestController {
     public ResponseEntity<?> findAll() {
         String page = request.getParameter("page");
         String limit = request.getParameter("limit");
-        return ResponseEntity.ok(service.findAllThuongHieu(page,limit));
+        return ResponseEntity.ok(service.findAllThuongHieu(page, limit));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ThuongHieuResponse> create(ThuongHieuRequest thuongHieuRequest) {
+        ThuongHieuResponse response = service.create(thuongHieuRequest);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ThuongHieuResponse> update(ThuongHieuRequest thuongHieuRequest){
+        ThuongHieuResponse response = service.update(thuongHieuRequest);
+        return ResponseEntity.ok(response);
     }
 
 }
