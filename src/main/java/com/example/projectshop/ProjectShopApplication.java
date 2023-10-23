8fa1b7ff8fd07060acfeeb77aa3ed9a629a02ade
Hưng Silver <hungsilvertq@gmail.com>
2023-10-23 18:16:35 +0700

    'upadate-nv-cv'

--- src/main/java/com/example/projectshop/ProjectShopApplication.java
+++ src/main/java/com/example/projectshop/ProjectShopApplication.java
@@ -8,18 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
-public class ProjectShopApplication implements CommandLineRunner {
+public class ProjectShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectShopApplication.class, args);
-    }
-
-    @Autowired
-    NhanVienRepository nv;
-
-    @Transactional
-    @Override
-    public void run(String... args) throws Exception {
-        nv.findAll().forEach(System.out::println);
     }
 }
