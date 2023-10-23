d97885ac7220cce2ec207a401f002062dea9aa40
Hưng Silver <hungsilvertq@gmail.com>
2023-10-23 15:45:59 +0700

    'xuatxu'

--- src/main/java/com/example/projectshop/repository/XuatXuRepository.java
+++ src/main/java/com/example/projectshop/repository/XuatXuRepository.java
@@ -1,13 +1,19 @@
 package com.example.projectshop.repository;
 
-import com.example.projectshop.domain.ChatLieuDeGiay;
 import com.example.projectshop.domain.Xuatxu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface XuatXuRepository extends JpaRepository<Xuatxu, Integer> {
     @Query(value = "select * from xuatxu  limit 1",nativeQuery = true)
     Xuatxu getTop1();
+
+    @Query("select x from Xuatxu x where x.ten like :ten")
+    Page<Xuatxu> findAllByTen(@Param("ten") String ten, Pageable pageable);
+
 }
--- src/main/java/com/example/projectshop/service/impl/XuatXuServiceImpl.java
+++ src/main/java/com/example/projectshop/service/impl/XuatXuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.projectshop.service.impl;
 
+import com.example.projectshop.domain.Xuatxu;
 import com.example.projectshop.dto.mausac.MauSacResponse;
 import com.example.projectshop.dto.xuatxu.XuatXuRequest;
 import com.example.projectshop.dto.xuatxu.XuatXuResponse;
@@ -13,45 +14,81 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class XuatXuServiceImpl implements IXuatXuService {
+public class XuatXuServiceImpl {
+//
+//    @Autowired
+//    private XuatXuRepository xuatXuRepository;
+//
+//    @Override
+//    public List<XuatXuResponse> getAll() {
+//        return null;
+//    }
+//
+//    @Override
+//    public XuatXuResponse findById(Integer id) {
+//
+//        return null;
+//    }
+//
+//    @Override
+//    public XuatXuResponse create(XuatXuRequest xuatXuRequest) {
+//        return null;
+//    }
+//
+//    @Override
+//    public XuatXuResponse update(XuatXuRequest xuatXuRequest, Integer id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void delete(Integer id) {
+//
+//    }
+
+//    @Override
+//    public Page<XuatXuResponse> findAllXuatXu(String pageParam, String limitParam) {
+//        Integer page = pageParam == null ? 0 : Integer.valueOf(pageParam);
+//        Integer limit = limitParam == null ? 3 : Integer.valueOf(limitParam);
+//        Pageable pageable = PageRequest.of(page, limit);
+//        Page<XuatXuResponse> list = ObjectMapperUtils.mapEntityPageIntoDtoPage(xuatXuRepository.findAll(pageable), XuatXuResponse.class);
+//        return list;
+//    }
+
 
     @Autowired
     private XuatXuRepository xuatXuRepository;
 
-    @Override
-    public List<XuatXuResponse> getAll() {
+    public Page<Xuatxu> findAll(Pageable pageable) {
+        return xuatXuRepository.findAll(pageable);
+    }
+
+    public Optional<Xuatxu> findById(Integer id) {
+        return xuatXuRepository.findById(id);
+    }
+
+    public Page<Xuatxu> findAllByName(String name, Pageable pageable) {
+        return xuatXuRepository.findAllByTen("%"+name+"%", pageable);
+    }
+
+    public Xuatxu create(Xuatxu xuatxu) {
+        xuatxu.setId(null);
+        return xuatXuRepository.save(xuatxu);
+    }
+
+    public Xuatxu update(Xuatxu xuatxu, Integer id) {
+        xuatxu.setId(id);
+        return xuatXuRepository.save(xuatxu);
+    }
+
+    public Xuatxu delete(Integer id) {
+        Optional<Xuatxu> xx = xuatXuRepository.findById(id);
+        if (xx.isPresent()) {
+            xx.get().setTrangThai(0);
+            return xuatXuRepository.save(xx.get());
+        }
         return null;
-    }
-
-    @Override
-    public XuatXuResponse findById(Integer id) {
-
-        return null;
-    }
-
-    @Override
-    public XuatXuResponse create(XuatXuRequest xuatXuRequest) {
-        return null;
-    }
-
-    @Override
-    public XuatXuResponse update(XuatXuRequest xuatXuRequest, Integer id) {
-        return null;
-    }
-
-    @Override
-    public void delete(Integer id) {
-
-    }
-
-    @Override
-    public Page<XuatXuResponse> findAllXuatXu(String pageParam, String limitParam) {
-        Integer page = pageParam == null ? 0 : Integer.valueOf(pageParam);
-        Integer limit = limitParam == null ? 3 : Integer.valueOf(limitParam);
-        Pageable pageable = PageRequest.of(page, limit);
-        Page<XuatXuResponse> list = ObjectMapperUtils.mapEntityPageIntoDtoPage(xuatXuRepository.findAll(pageable), XuatXuResponse.class);
-        return list;
     }
 }
